0775d617224ca3196b4b41a55349486a82cac968
Deep Kothari <deepkothari55@gmail.com>
2025-09-16 11:24:00 +0530

    correction

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,13 @@
 echo "BUILD START"
+
+# Install dependencies
 python3.9 -m pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput
+
+# Collect static files
+python3.9 manage.py collectstatic --noinput --clear
+
+# Create output folder for Vercel
+mkdir -p staticfiles_build
+cp -r staticfiles/* staticfiles_build/
+
 echo "BUILD END"
